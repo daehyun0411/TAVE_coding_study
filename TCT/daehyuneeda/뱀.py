963420c20a991f05ca2c3dfd572e202aa3a434c8
daehyun0411 <77386319+daehyun0411@users.noreply.github.com>
2022-05-31 18:35:14 +0900

    Add files via upload

---
+++ TCT/daehyuneeda/뱀.py
@@ -0,0 +1,15 @@
+n = int(input())
+k = int(input())
+
+array = [[0] * n for _ in range(n)]
+
+for _ in range(k):
+    r,c = map(int, input().split())
+    array[r-1][c-1] == 1
+   
+info = []  
+l = int(input())
+for _ in range(l):
+    x, c = input.split()
+    info.append(int(x), c)
+    
